ad66c8e527498241d6d1732ffe22f6cf80d1f5f4
adebudiman <art.visualdesigner@gmail.com>
2015-02-12 15:15:46 +0700

    Setipe.com
    Fixing bugs & finishing

--- assets/js/lib.js
+++ assets/js/lib.js
@@ -43,3 +43,21 @@ $(document).ready(function() {
     });
     
 });
+
+function fixTitle() {
+    $('.step').each(function () {
+        var $this = $(this);
+        var offset = $this.offset().top;
+        var scrollTop = $(window).scrollTop();
+
+        if (scrollTop > offset) {
+            $this.addClass('fixed');
+        } else {
+            $this.removeClass('fixed');
+        }
+    });
+}
+
+$(document).ready(function () {
+    $(window).scroll(fixTitle);
+});
